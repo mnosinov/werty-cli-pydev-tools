b77b8b719a07ed0cccd457026daf9dd0acbd52d8
werty <mnosinov@gmail.com>
2021-01-31 22:39:51 +0600

    test py files added

---
+++ some_no_logic_test_py_files/test1.py
@@ -0,0 +1,54 @@
+import os
+from os import dff
+
+class MyClass(Exception: class):
+    """
+    Some long long text. WwMmQgG1Ll0oOkKnNSTR
+    """
+    def __str__(self, *args, **kwargs)->str:
+        self.name = name
+        self.name = list()
+        return "name is:" + self.name
+
+@decorator(role="user1")
+def myfunc(myvar: dict)->list:
+    #this is comment
+    if True or False and not None and myvar != 234.555:
+        yield "some\n value\u789 "
+    else:  #TODO
+        for i in range(10):
+            print(i)
+    global a
+    a = {"my key": 12}
+    x = lambda a: a + 10
+    # Static files (CSS, JavaScript, Images)                                    asdfafd
+    nonlocal b
+    b = a + 22
+    print(a is b)
+    del b
+    s = "asfd.html asdffd. the_date. my.css. my_validatory"
+    assert True
+    return
+
+#23456789_123456789_123456789_123456789_123456789_123456789_123456789_123456789df
+THIS_IS_CONSTANT = 122222
+a = 777
+
+if __name__ == '__main__':
+    os.abc()
+    a = myfunc()
+    mycl = MyClass('my name')
+    os.path()
+    z = [1,2,3,4]
+    while z not None:
+        try:
+            break
+            continue
+        except Exception as myex:
+            print("error!")
+            raise myex
+        finally:
+            print(f"some f {a}, {THIS_IS_CONSTANT}")
+    with open("\\home\\pi\\.vimrc") as myfile:
+        r = myfile.read()
+
