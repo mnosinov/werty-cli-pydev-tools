549d4026acbe8ba371fe6cbd60171f699fdac81e
mnosinov <mnosinov@gmail.com>
2022-09-04 23:54:09 +0600

    everything rearranged and updated

--- setup/2_setup_console.sh
+++ setup/2_setup_console.sh
@@ -2,13 +2,13 @@ cd ..
 cp .inputrc ~
 cp .bashrc ~
 cp .tmux.conf ~
-cp .vimrc_v5 ~
-mv ~/.vimrc_v5 ~/.vimrc
+cp .vimrc_v7 ~
+mv ~/.vimrc_v7 ~/.vimrc
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 . install_vundle.sh
 mkdir ~/.vim
 mkdir ~/.vim/colors
 mkdir ~/.vim/spell
 cp ru* ~/.vim/spell
-cp werty-tty_v4.vim ~/.vim/colors
-mv ~/.vim/colors/werty-tty_v4.vim ~/.vim/colors/werty-tty.vim
+cp werty-tty_v6.vim ~/.vim/colors
+mv ~/.vim/colors/werty-tty_v6.vim ~/.vim/colors/werty-tty.vim
--- setup/4_post_manual_setup.sh
+++ setup/4_post_manual_setup.sh
@@ -3,6 +3,7 @@ cp badwolf_v3.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes
 mv ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/badwolf_v3.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/badwolf.vim 
 
 pip3 install flake8
+pip3 install wheel
 
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --all
