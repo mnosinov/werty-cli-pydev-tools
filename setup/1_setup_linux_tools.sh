60364b9a04c394773270a5f03ddea75ae46df2ef
mnosinov <mnosinov@gmail.com>
2022-09-08 19:22:30 +0600

    btop is accessible via snap. so it is not default apt packate, hence has been removed

--- setup/1_setup_linux_tools.sh
+++ setup/1_setup_linux_tools.sh
@@ -1,5 +1,5 @@
 sudo update
-sudo apt install -y konsole git zip unzip htop btop tree mc tmux vim vim-gtk vim-nox xclip build-essential cmake python3-dev mono-complete golang nodejs default-jdk npm silversearcher-ag ripgrep python3-pip exuberant-ctags python3-venv pgcli ncdu ranger neofetch
+sudo apt install -y konsole git zip unzip htop tree mc tmux vim vim-gtk vim-nox xclip build-essential cmake python3-dev mono-complete golang nodejs default-jdk npm silversearcher-ag ripgrep python3-pip exuberant-ctags python3-venv pgcli ncdu ranger neofetch
 
 
 
