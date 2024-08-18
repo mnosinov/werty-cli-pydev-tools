f54c76c103a59b4ecc4becabfc3ea63b3991db37
mirbek <mnosinov@gmail.com>
2024-08-18 12:01:41 +0500

    some updates

--- setup/1_setup_linux_tools.sh
+++ setup/1_setup_linux_tools.sh
@@ -1,5 +1,4 @@
-sudo update
-sudo apt install -y git zip unzip htop tree mc tmux vim vim-gtk vim-nox xclip build-essential cmake python2-dev python3-dev mono-complete golang nodejs default-jdk npm silversearcher-ag ripgrep python3-pip exuberant-ctags python3-venv pgcli ncdu ranger neofetch
-
+sudo apt update
+sudo apt install -y git zip unzip htop tree mc tmux vim vim-gtk3 vim-nox xclip build-essential cmake python3-dev mono-complete golang nodejs default-jdk npm silversearcher-ag ripgrep python3-pip exuberant-ctags python3-venv pgcli ncdu ranger neofetch
 
 
